330a6097891c366665ba644249202e76f36101ec
Srikanth Sathinathan <ssathinathan@criticalsoftware.com>
2024-01-16 13:18:32 +0000

    implement of api

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -3,19 +3,16 @@ import {test, expect} from '@playwright/test'
 test('API GET Request', async({request})=>{
     const response = await request.get('https://bc-sc-sqa-tt016.ocs-support.com/api/v1/reporting/schema/sconnect.v4.rawvolume/en',{
         headers:{
-            'x-api-key': '1f86c0c37dfd18792677a158ed9af9f6',
-            'Authorization':'Basic c2ljcGFjb25uZWN0OlMxY3A0QzBubjNjdA==',
-            'LocaleId':'fr-PT',
             'Content-Type':'application/json',
-            'X-AUTH-DEVICE-ID':'android_5012a17ff9a50815',
-            'Connection':'keep-alive',
-            'APP-VERSION':'1.4.2',
-            'Accept':'*/*',
-            'User-Agent':'PostmanRuntime/7.36.0',
-            'Postman-Token':'<calculated when request is sent>',
-            'Host':'<calculated when request is sent>',
-            
-        }
+            'X-AUTH-DEVICE-ID':'PCroSm5Vhb0u0t02LCGCc3Iok4cp',
+            'X-AUTH-OTP':'QoGCGKxsVWLxtsZ+cJgPioGtYfodVRKPl8kxx55xxCXMDW4W8ftCIEfrRUiy0HrHKNqp8BIf4L3vjlKvKBztvpm8lrIrT0zbud8E71FDyZI=,X-BlueCoat-Via=ccfe07c2148a837c',
+            'x-api-key': '1f86c0c37dfd18792677a158ed9af9f6',
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36',
+            'IP-ADDRESS':'10.40.38.16',
+            'APP-VERSION':'2.0.0',
+            'LocaleId':'en',
+            'x-api-key':'1f86c0c37dfd18792677a158ed9af9f6',    
+        },
     });
 
 })
