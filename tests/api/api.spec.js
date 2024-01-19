5cc44cfb5bfa5f6bb0c537cb31f9f7319f68e01a
Srikanth Sathinathan <ssathinathan@criticalsoftware.com>
2024-01-19 13:41:46 +0000

    update the api spec file

--- tests/api/api.spec.js
+++ tests/api/api.spec.js
@@ -19,22 +19,3 @@ test('API GET Request', async({request})=>{
     expect(responseBody.fullyQualifiedName).toBe('sconnect.v4.rawvolume')
     expect(responseBody.versionId).toBe('431')
 });
-
-test('API GET Request 1 ', async({request})=>{
-    const response = await request.get(baseURL,{
-        headers:{
-            'Content-Type':'application/json',
-            'X-AUTH-DEVICE-ID':'PCroSm5Vhb0u0t02LCGCc3Iok4cp',
-            'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36',
-            'IP-ADDRESS':'10.40.38.16',
-            'APP-VERSION':'2.0.0',
-            'LocaleId':'en',
-            'x-api-key': '1f86c0c37dfd18792677a158ed9af9f6', 
-            'X-AUTH-OTP':'QoGCGKxsVWLxtsZ+cJgPioGtYfodVRKPl8kxx55xxCXMDW4W8ftCIEfrRUiy0HrHKNqp8BIf4L3vjlKvKBztvpm8lrIrT0zbud8E71FDyZI=,X-BlueCoat-Via=ccfe07c2148a837c',
-        },
-    })
-    expect(response.status()).toBe(201)
-    const responseBody = JSON.parse(await response.text())
-    expect(responseBody.fullyQualifiedName).toBe('sconnect.v4.rawvolume')
-    expect(responseBody.versionId).toBe('431')
-});
