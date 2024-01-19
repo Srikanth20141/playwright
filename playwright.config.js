d0e13e6c5b44de30939b6c7ff21e36eb2c882d9e
Srikanth Sathinathan <ssathinathan@criticalsoftware.com>
2024-01-19 13:37:06 +0000

    update the palywright config file

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,8 @@ module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['dot'],
-    ['allure-playwright']
+    ['allure-playwright'],
+    ['html']
 ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
