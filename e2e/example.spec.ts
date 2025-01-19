426e9bf5f1cae5d7e9f3e391552497e5b94b68a3
Srikanth Sathinathan <srikanth.sathinathan@criticalsoftware.com>
2025-01-19 19:43:42 +0000

    test01

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -12,11 +12,5 @@ test('get started link', async ({ page }) => {
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
-  //test 01 in main
-  //dtest
-  //
-
-  //tst
-  // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
