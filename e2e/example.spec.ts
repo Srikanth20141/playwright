48a0989168c5c318b338a4c733775d1b55d4d4ab
Srikanth Sathinathan <srikanth.sathinathan@criticalsoftware.com>
2025-01-19 19:48:46 +0000

    Revert "test02"
    This reverts commit d04dcf6d4cd5b0dd58837d5fd6e2d841bcac0354.

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -9,7 +9,8 @@ test('has title', async ({ page }) => {
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
-//test01
